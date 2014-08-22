f1a0a273589a8f0efa49b23b4053a6688cdd3c31
aivic-unigedesk <ali.ivic88@gmail.com>
2014-08-22 14:35:30 +0200

    Cleaning the code a little bit.

--- src/ero2/resource/rest/ErO2RestRegistryResource.java
+++ src/ero2/resource/rest/ErO2RestRegistryResource.java
@@ -11,21 +11,21 @@ import ero2.core.ErO2Service;
 import ero2.util.ErO2JSON;
 
 public class ErO2RestRegistryResource extends ServerResource {
-	
-	ErO2Registry ERO2REGISTRY;
-	
-	public ErO2RestRegistryResource(){
-		ERO2REGISTRY = ErO2Registry.getInstance();
-	}
-	
-	@Get()
-	public String getServices() {
-		return serializeRegistry();
-	}
-	
-	private String serializeRegistry(){
-		Hashtable<String, ErO2Service> serviceRegistry=ERO2REGISTRY.allServices();
-		String servicesJSON=ErO2JSON.getServicesJSONString(serviceRegistry);
-		return servicesJSON;
-	}
+
+  ErO2Registry ERO2REGISTRY;
+
+  public ErO2RestRegistryResource(){
+    ERO2REGISTRY = ErO2Registry.getInstance();
+  }
+
+  @Get()
+  public String getServices() {
+    return serializeRegistry();
+  }
+
+  private String serializeRegistry(){
+    Hashtable<String, ErO2Service> serviceRegistry=ERO2REGISTRY.allServices();
+    String servicesJSON=ErO2JSON.getServicesJSONString(serviceRegistry);
+    return servicesJSON;
+  }
 }
--- src/ero2/util/ErO2JSON.java
+++ src/ero2/util/ErO2JSON.java
@@ -15,76 +15,76 @@ import ero2.core.ErO2ServiceStatus;
 
 public class ErO2JSON {
 
-	@SuppressWarnings("unchecked")
-	public static String getServicesJSONString(
-			Hashtable<String, ErO2Service> serviceRegistry) {
+  @SuppressWarnings("unchecked")
+  public static String getServicesJSONString(
+      Hashtable<String, ErO2Service> serviceRegistry) {
 
-		Enumeration<String> serviceKeys = serviceRegistry.keys();
+    Enumeration<String> serviceKeys = serviceRegistry.keys();
 
-		JSONObject finalJSON = new JSONObject();
-		JSONArray servicesJSON = new JSONArray();
-		while (serviceKeys.hasMoreElements()) {
-			String serviceLocator = serviceKeys.nextElement();
-			System.out.println(serviceLocator);
-			ErO2Service service = serviceRegistry.get(serviceLocator);
-			// []
-			Vector<ErO2Resource> resources = service.getResources();
-			JSONArray resourcesJSON = new JSONArray();
+    JSONObject finalJSON = new JSONObject();
+    JSONArray servicesJSON = new JSONArray();
+    while (serviceKeys.hasMoreElements()) {
+      String serviceLocator = serviceKeys.nextElement();
+      System.out.println(serviceLocator);
+      ErO2Service service = serviceRegistry.get(serviceLocator);
+      // []
+      Vector<ErO2Resource> resources = service.getResources();
+      JSONArray resourcesJSON = new JSONArray();
 
-			JSONObject resourceJSON;
-			for (ErO2Resource ero2Resource : resources) {
-				if (ero2Resource.getName() != null
-						&& ero2Resource.getMethod() != null) {
-					resourceJSON = new JSONObject();
-					resourceJSON.put("name", ero2Resource.getName());
-					resourceJSON.put("method", ero2Resource.getMethod());
-					resourceJSON.put("uri", ero2Resource.getURI());
-					resourceJSON.put("params",
-							ero2Resource.getQueryParameters());
-					resourcesJSON.add(resourceJSON);
-				}
-			}
+      JSONObject resourceJSON;
+      for (ErO2Resource ero2Resource : resources) {
+        if (ero2Resource.getName() != null
+            && ero2Resource.getMethod() != null) {
+          resourceJSON = new JSONObject();
+          resourceJSON.put("name", ero2Resource.getName());
+          resourceJSON.put("method", ero2Resource.getMethod());
+          resourceJSON.put("uri", ero2Resource.getURI());
+          resourceJSON.put("params",
+              ero2Resource.getQueryParameters());
+          resourcesJSON.add(resourceJSON);
+        }
+      }
 
-			// {service
-			JSONObject serviceJSON = new JSONObject();
-			serviceJSON.put("serviceID", serviceLocator);
-			serviceJSON.put("resources", resourcesJSON);
+      // {service
+      JSONObject serviceJSON = new JSONObject();
+      serviceJSON.put("serviceID", serviceLocator);
+      serviceJSON.put("resources", resourcesJSON);
 
-			servicesJSON.add(serviceJSON);
-		}
-		finalJSON.put("services", servicesJSON);
+      servicesJSON.add(serviceJSON);
+    }
+    finalJSON.put("services", servicesJSON);
 
-		StringWriter out = new StringWriter();
-		try {
-			finalJSON.writeJSONString(out);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String jsonText = out.toString();
-		System.out.println(jsonText);
-		return jsonText;
-	}
+    StringWriter out = new StringWriter();
+    try {
+      finalJSON.writeJSONString(out);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    String jsonText = out.toString();
+    System.out.println(jsonText);
+    return jsonText;
+  }
 
-	@SuppressWarnings("unchecked")
-	public static String getUpdatesJSONString(String serviceLocator,
-			ErO2ServiceStatus status) {
-		JSONObject readings = new JSONObject();
-		readings.put("service", serviceLocator);
-		Enumeration<String> readingsKeys = status.getReadings().keys();
-		while (readingsKeys.hasMoreElements()) {
-			String key = readingsKeys.nextElement();
-			readings.put(key, status.getReadings().get(key));
-		}
+  @SuppressWarnings("unchecked")
+  public static String getUpdatesJSONString(String serviceLocator,
+      ErO2ServiceStatus status) {
+    JSONObject readings = new JSONObject();
+    readings.put("service", serviceLocator);
+    Enumeration<String> readingsKeys = status.getReadings().keys();
+    while (readingsKeys.hasMoreElements()) {
+      String key = readingsKeys.nextElement();
+      readings.put(key, status.getReadings().get(key));
+    }
 
-		StringWriter out = new StringWriter();
-		try {
-			readings.writeJSONString(out);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String jsonText = out.toString();
-		return jsonText;
-	}
+    StringWriter out = new StringWriter();
+    try {
+      readings.writeJSONString(out);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    String jsonText = out.toString();
+    return jsonText;
+  }
 }
