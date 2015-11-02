c88b7987eda6bc68c92fb0957457c049697d2ad8
stefkundig <stefkundig@yahoo.gr>
2015-11-02 16:43:59 +0100

    Update ErO2XML.java

--- src/ero2/util/ErO2XML.java
+++ src/ero2/util/ErO2XML.java
@@ -14,7 +14,7 @@ import ero2.core.ErO2Resource;
 import ero2.core.ErO2Service;
 import ero2.core.ErO2ServiceStatus;
 
-public class ErO2XML2 {
+public class ErO2XML {
 
 	@SuppressWarnings("unchecked")
 	public String getServicesXMLString(
