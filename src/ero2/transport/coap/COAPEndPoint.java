21cefc60a6b8ade9c7643d0432459ece349c0231
stefkundig <stefkundig@yahoo.gr>
2015-12-09 12:27:37 +0100

    added check for expired resources task
    the task checks if the timestamp from the last heartbeat was too long ago

--- src/ero2/transport/coap/COAPEndPoint.java
+++ src/ero2/transport/coap/COAPEndPoint.java
@@ -1,13 +1,26 @@
 package ero2.transport.coap;
 
 import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import ch.ethz.inf.vs.californium.coap.Request;
 import ch.ethz.inf.vs.californium.endpoint.ServerEndpoint;
+import ero2.core.ErO2Registry;
+import ero2.core.ErO2Service;
 import ero2.resource.rest.ServiceMonitorResource;
 import ero2.resource.rest.ServiceRegistryResource;
 
+
 public class COAPEndPoint extends ServerEndpoint {
+	
+	//define here time of first check and check interval in milliseconds, when checking for expired resources
+	public static int FIRST_CHECK_OFFSET =180000; //3 min 
+	public static int CHECK_INTERVAL = 60000;  //1 min
 
 	public COAPEndPoint() throws SocketException {
 		// Initializes ErO2 services
@@ -18,8 +31,40 @@ public class COAPEndPoint extends ServerEndpoint {
 		// Add monitor service
 		ServiceMonitorResource monitorRegistry = new ServiceMonitorResource();
 		addResource(monitorRegistry);
+		
+		// Launch task periodically to check for sensors no longer reporting and remove them from registry
+		Timer t = new Timer();
+		t.scheduleAtFixedRate(
+		    new TimerTask()
+		    {
+		        public void run()
+		        {
+		        	System.out.println("Checking for removed sensors...");
+		        	ErO2Registry ERO2REGISTRY = ErO2Registry.getInstance();
+		        	Hashtable<String, ErO2Service> registry=ERO2REGISTRY.allServices();
+		        	Enumeration<String> serviceKeys = registry.keys();
+		        	while (serviceKeys.hasMoreElements()) {
+		        	      try {
+		        	    	  String serviceLocator = serviceKeys.nextElement();
+			        	      ErO2Service ero2ser = registry.get(serviceLocator);
+			        	      String now = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+			        	      if (!(now.substring(0, 13).equals(ero2ser.getTimestamp().substring(0, 13)))) { //HACK to check the date strings are equal until the desired accuracy - right now set to the hour
+			        	    	  registry.remove(serviceLocator);
+			        	    	  System.out.println("Sensor " + serviceLocator + " was no longer reporting and has been removed from registry");
+			        	      	  } 
+			        	      } catch (Exception e) {
+			        	    	  if (e instanceof NullPointerException){
+			        	    		  System.out.println("WARNING: caught NullPointerException when checking for sensor's last report time: timestamp has probably not been initialized");
+			        	    	  }
+			        	      }   	
+		    	    }
+		        }
+		    },
+		    FIRST_CHECK_OFFSET, 
+		    CHECK_INTERVAL); 
 	}
-
+	
+	
 	public void handleRequest(Request request) {
 		request.prettyPrint();
 		super.handleRequest(request);
